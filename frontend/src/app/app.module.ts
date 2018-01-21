8a6a3fd795a08d940ef910be6788559c49f7b06e
dpvnbd <dpvnbd@gmail.com>
2018-01-21 15:00:03 +0200

    Update to Angular 5, add token authentication package

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
 
+import {AppComponent} from './app.component';
+
+import {Angular2TokenService} from 'angular2-token';
+import {RouterModule} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
+import {HttpModule} from "@angular/http";
+
+
+const routes =[
+  { path: 'home', component: AppComponent}
+
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule, // Used in Angular2TokenService,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
   ],
-  providers: [],
+  providers: [Angular2TokenService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
